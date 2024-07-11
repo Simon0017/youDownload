9a0400eb2eaf3bea8b90343bc00fd4453fdb7b60
Simon Wekesa <wekesawafula15@gmail.com>
2024-07-11 18:58:00 +0300

    asgi chanegd

--- youDownload/asgi.py
+++ youDownload/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 
 import youDownloadapp.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE') #, 'core.settings')
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
